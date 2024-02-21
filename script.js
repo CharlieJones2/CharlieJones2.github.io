63045701162bea45200b89af77843040a284a93f
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-02-21 20:46:21 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header h1');
     header.textContent = 'Charlie Jones';
@@ -8,34 +6,38 @@ document.addEventListener('DOMContentLoaded', () => {
 const projects = [
     {
         name: 'Microsoft SQL Server into Power BI Dashboard',
-        description: 'For this project I\'m not entirely sure what I want to do yet, but I want it to use SQL Server and I want to create a Power BI dashboard.',
+        description: 'For this project I\'m not sure what I want to do yet, but I want it to use SQL Server and I want to create a Power BI dashboard.',
         softwareUsed: ['sql', 'powerbi'],
-        imgUrl: 'Images/Hotelier.png',
-        imgTitle: 'Project Image Title',
+        imgUrl: 'Images/Ethan.png',
+        imgTitle: 'Ethan (HGSS)',
+        link: "project_sqlserverpowerbi.html",
         softwareIcons: ['https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/microsoftsqlserver/microsoftsqlserver-original-wordmark.svg', 'https://raw.githubusercontent.com/microsoft/PowerBI-Icons/2bf1c982fb24528eee1559a96a25eb534c175cfd/SVG/Power-BI.svg']
     },
     {
         name: 'The Impact of Meat and Dairy on Climate Change',
         description: 'This project looks at one of the often most overlooked aspects of climate change and just how drastic its\' impact can be: eating animals.',
         softwareUsed: ['python', 'scikit-learn'],
-        imgUrl: 'Images/Wildfire.png',
-        imgTitle: 'Wildfire in Malibu',
+        imgUrl: 'Images/Corsola-Galar.png',
+        imgTitle: 'Galarian Corsola',
+        link: 'project_wildfires.html',
         softwareIcons: ['https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg']
     },
     {
         name: 'Project on Hardcore/Emo Music',
         description: 'Somehow creating a project on Hardcore or Emo - I don\'t really want it to be the same as any other Spotify project out there but not sure what to do.',
         softwareUsed: ['python', 'pytorch', 'powerbi'],
-        imgUrl: 'Images/TitleFight.png',
-        imgTitle: 'Title Fight',
+        imgUrl: 'Images/Roxie.png',
+        imgTitle: 'Roxie',
+        link: 'project_music.html',
         softwareIcons: ['https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg', 'https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg', 'https://raw.githubusercontent.com/microsoft/PowerBI-Icons/2bf1c982fb24528eee1559a96a25eb534c175cfd/SVG/Power-BI.svg']
     },
     {
         name: 'Biodiversity in National Parks',
         description: 'This project analyses biodiversity data across national parks in the USA, looking specifically at endangered species across different parks.',
         softwareUsed: ['python', 'scikit-learn'],
-        imgUrl: 'Images/Yosemite.png',
-        imgTitle: 'Yosemite National Park',
+        imgUrl: 'Images/Celebi.png',
+        imgTitle: 'Celebi',
+        link: 'project_biodiversity.html',
         softwareIcons: ['https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg']
     },
     {
@@ -44,6 +46,7 @@ const projects = [
         softwareUsed: ['python', 'tableau'],
         imgUrl: 'Images/Bulbasaur.png',
         imgTitle: 'Bulbasaur',
+        link: 'project_pokemon.html',
         softwareIcons: ['https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg', 'https://upload.vectorlogo.zone/logos/tableau/images/113a311a-6d5d-4b7e-9193-79807e4844e3.svg']
     }
 ];
@@ -51,11 +54,10 @@ const projects = [
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const projectsContainer = document.querySelector('.projects');
 
-function show_image(src, title, style) {
+function show_image(src, title) {
     var img = document.createElement("img");
     img.src = src;
     img.title = title;
-    img.style = style;
     return img;
 }
 
@@ -68,24 +70,46 @@ function filterProjects() {
 
     projects.forEach((project) => {
         if (selectedSoftware.every((software) => project.softwareUsed.includes(software))) {
-            // Show the project
+            // Create a project element
             const projectElement = document.createElement('div');
             projectElement.classList.add('project');
             projectElement.innerHTML = `
                 <h2>${project.name}</h2>
                 <p>${project.description}</p>
             `;
-            project.softwareIcons.forEach((softwareicons) => {
-                const iconImage = show_image(softwareicons);
-            projectElement.appendChild(iconImage);
+    
+            // Create a container for software icons
+            const iconsContainer = document.createElement('div');
+            iconsContainer.classList.add('icons-container');
+    
+            // Add software icons
+            project.softwareIcons.forEach((softwareIcon) => {
+                const iconImage = show_image(softwareIcon);
+                iconImage.classList.add('icon-image');
+                iconsContainer.appendChild(iconImage);
             });
-
-            //attempting to distinguish the project image from the icons
+    
+            // Create a container for the project image
+            const imageContainer = document.createElement('div');
+            imageContainer.classList.add('image-container');
+    
+            // Add project image
             const projectImage = show_image(project.imgUrl, project.imgTitle);
             projectImage.classList.add('project-image');
-
-            projectElement.appendChild(projectImage)
-            projectsContainer.appendChild(projectElement);
+            imageContainer.appendChild(projectImage);
+    
+            // Append the containers to the project element
+            projectElement.appendChild(iconsContainer);
+            projectElement.appendChild(imageContainer);
+    
+            // Create an anchor tag for the project link
+            const projectLink = document.createElement('a');
+            projectLink.href = project.link; // Set the link URL
+            projectLink.style.display = 'contents'; // Makes only the box clickable
+            projectLink.appendChild(projectElement); // Wrap the project element
+    
+            // Append the entire project to the container
+            projectsContainer.appendChild(projectLink);
         }
     });
 }
